aa193b4f2cd9512ee920772088bc257a6c716e03
jardenliu <jarden@qq.com>
2021-06-21 20:02:04 +0800

    update target

--- ipadpro/index.js
+++ ipadpro/index.js
@@ -24,7 +24,7 @@ const resolveHtml = async (html) => {
         $('div.refurbished-category-grid-no-js li h3 a').each(function () {
             const text = $(this).text()
             console.log('节点获取正常', text)
-            if (~text.indexOf('12.9') && ~text.indexOf('第三代')) {
+            if (~text.indexOf('12.9') && ~text.indexOf('第四代')) {
                 resolve(true)
             }
         })
